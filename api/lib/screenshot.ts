3ee5f8289c4eaed79b1e717ebe2d7b1f44320fb5
Jonas Daniels <jonas.daniels@outlook.com>
2022-09-08 12:22:55 -0700

    attempt to disable the -dev-shm-usage flag

--- api/lib/screenshot.ts
+++ api/lib/screenshot.ts
@@ -12,8 +12,6 @@ type PuppeteerLaunchOptions = LaunchOptions &
   BrowserConnectOptions;
 
 export default async function screenshot(url: string) {
-  console.time("screenshot-e2e");
-  console.time("browser-launch");
   const options: PuppeteerLaunchOptions = isDev
     ? {
         args: [],
@@ -26,26 +24,22 @@ export default async function screenshot(url: string) {
             : "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
       }
     : {
-        args: chrome.args,
+        args: [...chrome.args, "--disable-dev-shm-usage"],
         executablePath: await chrome.executablePath,
         headless: chrome.headless,
         devtools: false,
       };
 
   const browser = await puppeteer.launch(options);
-  console.timeEnd("browser-launch");
-  console.time("page-setup");
+
   const page = await browser.newPage();
   // og image is supposed to be 1.9:1 (1200x630)
   // twitter image is supposed to be 2:1 (1200x600), but works fine with 1200x630, too
   await page.setViewport({ width: 1200, height: 630, deviceScaleFactor: 2 });
-  console.timeEnd("page-setup");
-  console.time("page-load");
+
   await page.goto(url, { waitUntil: "networkidle2" });
-  console.timeEnd("page-load");
-  console.time("take-screenshot");
+
   const screenshot = await page.screenshot({ type: "png" });
-  console.timeEnd("take-screenshot");
-  console.timeEnd("screenshot-e2e");
+
   return screenshot;
 }
